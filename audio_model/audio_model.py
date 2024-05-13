26d8205127001360d3ffc1c4b37eb1ed899851a0
robbagott <robbagott@gmail.com>
2024-05-13 17:19:26 -0400

    Added batch normalization to the audio model and improved results slightly to 85% contains acc and 77% exact acc

--- audio_model/audio_model.py
+++ audio_model/audio_model.py
@@ -6,37 +6,27 @@ import torch.nn.functional as F
 class AudioModel(nn.Module):
     def __init__(self):
         super().__init__()
-        # self.fc1 = nn.Linear(300, 300)
-        # self.fc2 = nn.Linear(300, 300)
-        # self.fc3 = nn.Linear(300, 300)
-        # self.fc4 = nn.Linear(300, 150)
-        # self.fc5 = nn.Linear(150, 49)
+        self.sequential = nn.Sequential(
+          nn.Linear(450, 450),
+          nn.BatchNorm1d(450),
+          nn.ReLU(),
+          nn.Dropout(0.2),
+          nn.Linear(450, 300),
+          nn.BatchNorm1d(300),
+          nn.ReLU(),
+          nn.Dropout(0.2),
+          nn.Linear(300, 300),
+          nn.BatchNorm1d(300),
+          nn.ReLU(),
+          nn.Dropout(0.2),
+          nn.Linear(300, 150),
+          nn.BatchNorm1d(150),
+          nn.ReLU(),
+          nn.Dropout(0.2),
+          nn.Linear(150, 49)
+        )
 
-        # self.fc1 = nn.Linear(450, 450)
-        # self.fc2 = nn.Linear(450, 300)
-        # self.fc3 = nn.Linear(300, 300)
-        # self.fc4 = nn.Linear(300, 150)
-        # self.fc5 = nn.Linear(150, 49)
-
-        self.fc1 = nn.Linear(450, 450)
-        self.fc2 = nn.Linear(450, 300)
-        self.fc3 = nn.Linear(300, 300)
-        self.fc4 = nn.Linear(300, 150)
-        self.fc5 = nn.Linear(150, 49)
-
-        # self.fc1 = nn.Linear(750, 500)
-        # self.fc2 = nn.Linear(500, 250)
-        # self.fc3 = nn.Linear(250, 49)
-
-        self.dropout = nn.Dropout(0.2)
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc3(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc4(x))
-        x = self.dropout(x)
-        x = F.sigmoid(self.fc5(x))
+        x = self.sequential(x)
+        x = F.sigmoid(x)
         return x
